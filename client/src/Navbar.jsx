33ed3ecf2eb454b878a2b2bb1c94cb581d2b898c
b19karhj <sc2rena@gmail.com>
2024-08-02 16:28:32 +0200

    Navbar dropdown working
    Navbar dropdown function working

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,24 +1,34 @@
+import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
+import "./navBar.css";
 
 export function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggle = () => {
+    setIsActive(!isActive);
+  };
+
+  const dropDown = isActive ? "main-nav__item active" : "main-nav__item";
+
   return (
     <>
       <header className="main-header">
         <nav className="main-nav">
-          <ul className="main-nav__items">
+          <ul className={`main-nav__items ${isActive} ? active : " " `}>
             <li className="main-brand">
               <Link to="/index">RezData</Link>
             </li>
-            <li className="main-nav__item">
+            <li className={dropDown}>
               <Link to="/index">Home</Link>
             </li>
-            <li className="main-nav__item">
+            <li className={dropDown}>
               <Link to="/index">Employees</Link>
             </li>
-            <li className="main-nav__item">
+            <li className={dropDown}>
               <Link to="/index">About</Link>
             </li>
-            <li className="main-nav__toggle">
+            <li className="main-nav__toggle" onClick={toggle}>
               <span className="main-nav__bars"></span>
             </li>
           </ul>
